4dbf737deb82f09d67df85bee8c888f92cae9ca9
Đinh Đức Dương <97863053+dinhducduong@users.noreply.github.com>
2023-06-16 10:19:51 +0700

    ud

--- tap_bigcommerce/__init__.py
+++ tap_bigcommerce/__init__.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import datetime
 import sys
 import json
 import singer
@@ -48,7 +47,7 @@ def ensure_credentials_are_authorized(client):
         raise Exception("BigCommerce Client not authorized.")
 
 
-def do_sync(client, catalog, state, start_date):
+def do_sync(client, catalog, state):
     ensure_credentials_are_authorized(client)
     selected_stream_names = get_selected_streams(catalog)
     populate_class_schemas(catalog, selected_stream_names)
@@ -80,7 +79,6 @@ def do_sync(client, catalog, state, start_date):
                 stream.tap_stream_id, {}
             ).get(instance.replication_key) is None:
                 state['bookmarks'][stream.tap_stream_id] = {
-                    instance.replication_key: start_date
                 }
 
         counter_value = sync_stream(state, instance)
@@ -108,7 +106,7 @@ def main():
         access_token=config['access_token'],
         store_hash=config['store_hash']
     )
-    start_date = datetime.datetime.now()
+
     # If discover flag was passed, run discovery mode and dump output to stdout
     if args.discover:
         do_discover(bigcommerce)
@@ -122,8 +120,7 @@ def main():
         do_sync(
             client=bigcommerce,
             catalog=catalog,
-            state=args.state,
-            start_date= str(start_date)
+            state=args.state
         )
 
 
